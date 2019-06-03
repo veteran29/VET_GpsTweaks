59e070c3c2ac97d8e30d5825ccb4691edf57ce25
Filip Maciejewski <veteran29q@gmail.com>
2019-06-03 09:21:45 +0200

    Use macro inside GpsSettings class

--- addons/gps/GpsSettings.hpp
+++ addons/gps/GpsSettings.hpp
@@ -1,20 +1,22 @@
 #include "settingsDefaults.hpp"
 
+#define GPS_SETTING(NAME)   GVAR(NAME) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(NAME)', DEFAULT)])
+
 class GVAR(GpsSettings) {
     // Zoom
-    GVAR(gpsZoom) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(gpsZoom)', GPS_ZOOM_DEFAULT)]);
-    GVAR(gpsSpeedCoef) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(gpsSpeedCoef)', GPS_SPEED_COEF_DEFAULT)]);
+    GPS_SETTING(gpsZoom, GPS_ZOOM_DEFAULT);
+    GPS_SETTING(gpsSpeedCoef, GPS_SPEED_COEF_DEFAULT);
     // Road colors
-    GVAR(colorRoads_R) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(colorRoads_R)', GPS_ROAD_R_DEFAULT)]);
-    GVAR(colorRoads_G) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(colorRoads_G)', GPS_ROAD_G_DEFAULT)]);
-    GVAR(colorRoads_B) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(colorRoads_B)', GPS_ROAD_B_DEFAULT)]);
+    GPS_SETTING(colorRoads_R, GPS_ROAD_R_DEFAULT);
+    GPS_SETTING(colorRoads_G, GPS_ROAD_G_DEFAULT);
+    GPS_SETTING(colorRoads_B, GPS_ROAD_B_DEFAULT);
     // Raod alpha
-    GVAR(roads_A) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(roads_A)', GPS_ROAD_A_DEFAULT)]);
-    GVAR(roadsFill_A) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(roadsFill_A)', GPS_ROAD_FILL_A_DEFAULT)]);
+    GPS_SETTING(roads_A, GPS_ROAD_A_DEFAULT);
+    GPS_SETTING(roadsFill_A, GPS_ROAD_FILL_A_DEFAULT);
     // Tracks alpha
-    GVAR(tracks_A) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(tracks_A)', GPS_TRACKS_A_DEFAULT)]);
-    GVAR(tracksFill_A) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(tracksFill_A)', GPS_TRACKS_FILL_A_DEFAULT)]);
+    GPS_SETTING(tracks_A, GPS_TRACKS_A_DEFAULT);
+    GPS_SETTING(tracksFill_A, GPS_TRACKS_FILL_A_DEFAULT);
     // Main roads alpha
-    GVAR(mainRoads_A) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(mainRoads_A)', GPS_MAINROAD_A_DEFAULT)]);
-    GVAR(mainRoadsFill_A) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(mainRoadsFill_A)', GPS_MAINROAD_FILL_DEFAULT)]);
+    GPS_SETTING(mainRoads_A, GPS_MAINROAD_A_DEFAULT);
+    GPS_SETTING(mainRoadsFill_A, GPS_MAINROAD_FILL_DEFAULT);
 };
